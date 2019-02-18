fb1749c5a4e7620a7738672b39e875d5751c5d21
J-Azuma <azumanotdetail+2@gmail.com>
2019-02-18 23:37:35 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/about', to: 'home#about'
   get '/contact', to: 'home#contact'
   get '/signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
