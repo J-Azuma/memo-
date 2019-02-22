b4f2a981d4e69a26655c1f4d4a11bda8a06ff823
J-Azuma <azumanotdetail+2@gmail.com>
2019-02-22 23:40:29 +0900

    Finish user edit, update, indeex, and. destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  #なぜindexページをログインユーザー限定にする？
+  before_action :logged_in_user, only:[:edit, :update, :index]
+  before_action :admin_user, only: :destroy
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -18,8 +25,46 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+   if @user.update_attributes(user_params)
+     flash[:success] = "Profile updated!"
+     redirect_to @user
+   else
+     render 'edit'
+   end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User updated"
+    redirect_to users_url
+  end
+
   private
   def user_params
+    #セキュリティの都合上、admin属性は編集が許可されていない
     params.require(:user).permit(:name, :email, :password, :password_cofirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location 
+      flash[:danger] = "Please log in."
+      redirect_to root_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(roo_url) unless current_user.admin?
   end
 end
