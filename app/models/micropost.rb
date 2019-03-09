657d5ba8909d45c062259dccb2165a7b31f0f6cf
J-Azuma <azumanotdetail+2@gmail.com>
2019-03-09 13:04:25 +0900

    Add micropost like

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,6 +5,20 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 150}
   validate :picture_size
+  has_many :likes, dependent: :destroy
+  has_many :fav_users, through: :likes, source: :user
+
+  def fav(user)
+    likes.create(user_id: user.id)
+  end
+
+  def unfav(user)
+    likes.find_by(user_id: user.id).destroy
+  end
+
+  def fav?(user)
+    fav_users.include?(user)
+  end
 
   private
 
---
+++ db/migrate/20190309023316_add_likes_count_to_microposts.rb
@@ -0,0 +1,36 @@
+class AddLikesCountToMicroposts < ActiveRecord::Migration[5.2]
+  #def change
+    #add_column :microposts, :likes_count, :integer
+  #end
+
+  #冪等性を確保するためにup methodとdown methodで処理を分割？
+  #冪等性＝同じ処理を何回行っても結果が同じになること。
+  class MigrationUser < ApplicationRecord
+   self.table_name = :microposts
+  end
+
+  def up 
+    _up
+  rescue => e 
+    _down 
+    raise e
+  end
+
+  def down
+    _down
+  end
+
+  private 
+
+  def _up
+    MigrationUser.reset_column_information
+    add_column :microposts, :likes_count, :integer, null: false, 
+    default: 0 unless column_exists? :microposts, :likes_count 
+  end
+
+  def _down
+    MigrationUser.reset_column_information 
+    remove_column :microposts, :likes_count if column_exists? :microposts, :likes_count
+  end
+  
+end
